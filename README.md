6ff9a01fa23b5a5b4934295cbb252bd56e13b3ec
kas.goto <meiliunasvilius@gmail.com>
2024-09-14 03:10:06 +0300

    overflow banner fix

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Technologies used: React, Vite, Tailwind, Framer motion
 
+Live demo: https://kasgoto.github.io/fashion/
+
 - ![image](https://github.com/user-attachments/assets/947305b5-7742-4dc6-ad7e-2c701fc66608)
 
 - Figma layout: https://www.figma.com/file/My6lLGRWRc9R0Jj1TPL8ZA/Shopping-Website-(Community)---Update?type=design&node-id=2%3A2&t=TLLYgyM5xMlsZgEi-1
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,13 +2,13 @@ import ImgBanner from "../assets/images/promo-img.jpg"
 
 const Banner = () => {
   return (
-    <div className='max-w-[1920px] w-screen mt-[60px] md:mt-[143px] min-h-[700px] md:min-h-[900px] 2xl:min-h-fit flex items-center justify-center mx-auto bg-banner-gradient overflow-hidden'>
+    <div className='max-w-[1895px] w-screen mt-[60px] md:mt-[143px] min-h-[700px] md:min-h-[900px] 2xl:min-h-fit flex items-center justify-center mx-auto bg-banner-gradient overflow-hidden'>
       <img
         src={ImgBanner}
         alt='banner image'
         className='max-h-full w-1/2 2xl:block hidden'
       />
-      <div className='md:pl-[100px] px-[25px] pt-3 md:px[10px] flex-1 w-full h-full mt-7 md:my-auto overflow-hidden'>
+      <div className='md:pl-[100px] pl-[25px] pt-3 md:pl[10px] flex-1 w-full h-full mt-7 md:my-auto overflow-hidden'>
         <h1 className='md:text-[96px] text-[60px] font-black leading-tight max-w-full md:max-w-[630px]'>
           <span className='before:block before:absolute before:-inset-1 before:-skew-y-2 before:skew-x-2 before:w-[280px] before:h-[80px] before:ml-[-5px] md:before:w-[474px] md:before:h-[122px] md:before:ml-[-20px] before:bg-white relative inline-block'>
             <span className='relative'>PAYDAY</span>
@@ -16,7 +16,7 @@ const Banner = () => {
           <br />
           SALE NOW
         </h1>
-        <p className='my-[40px] font-medium text-[26px] md:text-[32px] tracking-tighter lg:w-full text-[#231300]'>
+        <p className='my-[40px] font-medium text-[26px] md:text-[32px] tracking-tighter lg:max-w-[590px] text-[#231300]'>
           Spend minimal $100 get 30% off <br />
           voucher code for your next purchase
         </p>
